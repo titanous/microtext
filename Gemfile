2d133e84b26ffbaf6e80bd2fc276ab2b2b6c7e1a
Jonathan Rudenberg <jonathan@titanous.com>
2012-05-10 13:17:31 -0400

    Add asset support, jquery, batman

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :rubygems
 
 gem 'sinatra-synchrony'
 gem 'sinatra-contrib'
+gem 'sinatra-assetpack', :git => 'https://github.com/rstacruz/sinatra-assetpack.git'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'haml'
--- app.rb
+++ app.rb
@@ -3,15 +3,27 @@ require 'bundler/setup'
 require 'sinatra/base'
 require 'sinatra/synchrony'
 require 'sinatra/reloader'
+require 'sinatra/assetpack'
 require 'haml'
 require './models'
 
 class Microtext < Sinatra::Base
   register Sinatra::Synchrony
+  register Sinatra::AssetPack
   use Rack::CommonLogger
 
   configure :development do
     register Sinatra::Reloader
+  end
+
+  assets do
+    serve '/js',  from: 'static/js'
+    serve '/css', from: 'static/css'
+
+    js :app, '/js/app.js', ['/js/*.js']
+
+    js_compression  :jsmin
+    css_compression :sass
   end
 
   get '/' do
